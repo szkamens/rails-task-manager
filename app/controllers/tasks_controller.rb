b3d34ff201d28add227247623eb38b82160fcfb3
Schneur Kamensky <szkamens@gmail.com>
2020-05-11 14:32:10 +0300

    5 - add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,24 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
   private
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
